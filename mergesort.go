93602a98969302820bd766d3bee7f3ecf2b7f383
SL4SH73R <87204613+sl4sh73r@users.noreply.github.com>
2022-10-07 02:59:29 +0300

    Update mergesort.go

--- mergesort.go
+++ mergesort.go
@@ -6,30 +6,44 @@ import (
 	"time"
 )
 
-func mergeSort(items [][]int) [][]int {
-	if len(items) < 2 {
-		return items
-	}
-	first := mergeSort(items[:len(items)/2])
-	second := mergeSort(items[len(items)/2:])
-	return merge(first, second)
+type customStruct struct {
+	stack [][]int
+	size  int
 }
 
-func size(a []int) int {
+func LRandint(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+func sise(a []int) int {
 	return len(a)
 }
 
-func Randint(min int, max int) int {
-
-	return min + rand.Intn(max-min)
+func PushFront(size, value int) [][]int {
+	c := customStruct{size: size}
+	m := make([][]int, c.size)
+	for i := 0; i < len(m); i++ {
+		t := LRandint(1, len(m))
+		for j := 0; j < t; j++ {
+			m[i] = append(m[i], value)
+		} //create massive
+		if len(m) > 1 {
+			for h := 0; h < len(m); h++ {
+				if len(m[i]) < 1 {
+					m = append(m[:h], m[h+1:]...)
+					h--
+				} //reversed massive
+			}
+		}
+	}
+	return m
 }
 
-func merge(a [][]int, b [][]int) [][]int {
+func mergen(a [][]int, b [][]int) [][]int { //double merge sort
 	final := make([][]int, 1)
 	i := 0
 	j := 0
 	for i < len(a) && j < len(b) {
-		if size(a[i]) < size(b[j]) {
+		if sise(a[i]) < sise(b[j]) {
 			final = append(final, a[i])
 			i++
 		} else {
@@ -46,45 +60,33 @@ func merge(a [][]int, b [][]int) [][]int {
 	return final
 }
 
-func createMssive(size int) [][]int {
-	m := make([][]int, size)
-	reversed := make([][]int, size)
-	for i := 0; i < size; i++ {
-		t := Randint(1, size)
-		for j := 0; j < t; j++ {
-			m[i] = append(m[i], 1)
-		}
+func mergeSorti(items [][]int) [][]int {
+	if len(items) < 2 {
+		return items
 	}
-	for i := range m {
-		n := m[len(m)-1-i]
-		//fmt.Println(n) -- sanity check
-		reversed = append(reversed, n)
-	}
-	return reversed
+	first := mergeSorti(items[:len(items)/2])
+	second := mergeSorti(items[len(items)/2:])
+	return mergen(first, second)
 }
 
 func main() {
+
 	var n int
 	fmt.Scanln(&n)
 	now := time.Now()
-	unsorted := createMssive(n)
-	fmt.Println("unsorted")
-	for i := 0; i < len(unsorted); i++ {
-		if len(unsorted[i]) < 1 {
-			unsorted = append(unsorted[:i], unsorted[i+1:]...)
-			i--
-		}
-	}
-	fmt.Println(unsorted)
-	sorted := mergeSort(unsorted)
+	c := customStruct{stack: PushFront(n, 1), size: n}
+	fmt.Println(c.stack)
+	sorted := mergeSorti(c.stack)
 	for i := 0; i < len(sorted); i++ {
 		if len(sorted[i]) < 1 {
 			sorted = append(sorted[:i], sorted[i+1:]...)
 			i--
 		}
 	}
-	fmt.Println("sorted")
-	fmt.Println(sorted)
 	durat := time.Since(now)
+	fmt.Println(sorted)
 	fmt.Println(durat)
+	//TODO an OP как считать кол-во операций в разных функциях?
+	//TODO посчитать big O
+
 }
